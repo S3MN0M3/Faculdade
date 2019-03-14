68d1945a7d3c59915f48da2d10303bc352fdc4e2
Gabriel <gabrielwinter1306@gmail.com>
2019-03-14 18:54:41 -0300

    3.3

--- Exercicio3.3.cpp
+++ Exercicio3.3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 /*
-3) FAÇA UM ALGORITIMO QUE RECEBA O VALOR DO DEPOSITO E A QUANTIDADE DE MESES QUE O DEPOSITO FOI
+3) FA�A UM ALGORITIMO QUE RECEBA O VALOR DO DEPOSITO E A QUANTIDADE DE MESES QUE O DEPOSITO FOI
 FEITO
 SE
 - O VALOR DE DEPOSITO >= 500 E MESES >= 10 APLICA 10% DE JUROS
@@ -22,15 +22,34 @@ int main() {
 	cout << "Digite a quantidade de meses: " << endl;
 	cin >> meses;
 	
-	
+	// Testar com else{
+	// if
 	// && == and
 	if ( valor >= 500 && meses >= 10 ){
 		float juros1 = valor * 10 / 100;
 		float juros1_total = valor + juros1;
-		cout << "Valor com 10% de juros: " << juros1_total;
-	}	
-	else{
-		cout << "Funcinou";
+		cout << "Valor com 10% de juros: " << juros1_total << " Juros: " << juros1 << endl;
 	}
+		
+	else if ( valor >= 500 && meses < 10){
+		float juros2 = valor * 8 / 100;
+		float juros2_total = valor + juros2;
+		cout << "Valor com 8% de juros: " << juros2_total << " Juros: " << juros2 << endl;
+	}
+	else if ( valor < 500 && meses >= 10 ){
+		float juros3 = valor * 4 / 100;
+		float juros3_total = valor + juros3;
+		cout << "Valor com 4% de juros: " << juros3_total << " Juros: " << juros3 << endl;				
+	}
+	else if ( valor < 500 && meses < 10 ){
+			float juros4 = valor * 2 / 100;
+			float juros4_total = valor + juros4;
+			cout << "Valor com 2% de juros: " << juros4_total << " Juros: " << juros4 << endl;
+	}
+	else
+	{
+		cout << "Deu Erro!" << endl;
+	}
+	
 	return 0;
 }
---
+++ Exercicio3.4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/*
+Faça um programa em C++ que recebe 5 valores.
+a = 67, b = 67.534, c = 13.657, d = 56.623, e = 24
+Imprimir o valor b com 1 casa decimal;
+Imprimir o valor c com 2 casas decimais;
+Imprimir o valor a como caracter;
+Imprimir a multiplicação de c, d, e. Somente com 2 casas decimais;
+Imprimir a divisão de d por e. Com 1 casa decimal.
+Imprimir o resto da divisão de a por e.
+Imprimir a raiz quadrada de c, com 2 casas decimais;
+Imprimir a subtração de b - c;
+Imprimir a soma de a, b, c, d, com 1 casa decimal;
+Imprimir a multiplicação de c, e, sem casa decimal.
+*/
+
+using namespace std;
+
+int main() {
+    int a = 67;
+    float b = 67.534;
+    float c = 13.657;
+    float d = 56.623;
+    int e = 24;
+
+    float teste_b = ("{:.2f}";
+    cout << teste_b << endl;
+
+	return 0;
+}
